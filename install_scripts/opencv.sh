8f579cb843aa676bd7ea7e033bc67844fdca31dd
Thomas Krause <thomaskrause@posteo.de>
2016-02-02 14:00:14 +0100

    disable some unused functions

--- install_scripts/opencv.sh
+++ install_scripts/opencv.sh
@@ -24,8 +24,9 @@ elif [ "$1" = "install" ]; then
   rm -Rf opencv-3.0.0
   tar xvzf ../downloads/opencv-3.1.0.tar.gz
   cd opencv-3.1.0
+  rm -Rf build
   mkdir build && cd build
-  cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX="$EXTERN_DIR" ..
+  cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX="$EXTERN_DIR" -D BUILD_opencv_apps=off -D BUILD_DOCS=off -D BUILD_TESTS=off -D BUILD_opencv_java=off -D BUILD_opencv_python2=off BUILD_opencv_python3=off -D BUILD_PERF_TESTS=off ..
   make && make install
   cd ..  
 fi
