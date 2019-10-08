db8d160e2df7c77a5107a5a91f99470a9269ebee
Philipp Strobel <philippstrobel@posteo.de>
2019-10-08 09:45:21 +0200

    define the fftw include directory directly in the configure command

--- toolchain_native/extern/install_scripts/fftw3.sh
+++ toolchain_native/extern/install_scripts/fftw3.sh
@@ -3,6 +3,8 @@
 # stop at the first error
 set -e
 
+FFTW_INCLUDE_DIR='include/fftw3'
+
 # current path is toolchain_native/extern/extracted
 
 if [ "$EXTERN_DIR" == "" ]; then
@@ -12,7 +14,7 @@ if [ "$EXTERN_DIR" == "" ]; then
 fi
 
 if [ "$1" = "check" ]; then
-  if [ -f "/usr/include/fftw3.h" -o -f "$EXTERN_DIR/include/fftw3.h" ]; then
+  if [ -f "/usr/include/fftw3.h" -o -f "$EXTERN_DIR/$FFTW_INCLUDE_DIR/fftw3.h" ]; then
     echo "n"
     exit 0
   else
@@ -23,16 +25,6 @@ elif [ "$1" = "install" ]; then
   rm -Rf fftw-3.3.5
   tar xvzf ../downloads/fftw-3.3.5.tar.gz
   cd fftw-3.3.5
-  ./configure --prefix="$EXTERN_DIR" && make && make install
-  cd ..  
-
-  # move fftw headerfiles in separate folder as the naoth code expects
-  cd ../include
-  mkdir -p fftw3
-  mv fftw3.h  fftw3/fftw3.h
-  mv fftw3.f03 fftw3/fftw3.f03
-  mv fftw3.f fftw3/fftw3.f
-  mv fftw3l.f03 fftw3/fftw3l.f03
-  mv fftw3q.f03 fftw3/fftw3q.f03
-  cd ../extracted
+  ./configure --prefix="$EXTERN_DIR" --includedir="\$(prefix)/$FFTW_INCLUDE_DIR" && make && make install
+  cd ..
 fi
