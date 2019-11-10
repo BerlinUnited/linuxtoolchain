3ff60e5573e6c56fda62f9331d8053246ec86fc0
Philipp Strobel <philippstrobel@posteo.de>
2019-11-10 09:56:16 +0100

    fixed the include path (see infrastructure/jpeg)

--- toolchain_native/extern/install_scripts/turbojpeg.sh
+++ toolchain_native/extern/install_scripts/turbojpeg.sh
@@ -12,7 +12,7 @@ fi
 if [ "$1" = "check" ]; then
   # don't check global install since we have to be sure to have the same version of protobuf on
   # our computer as the one on the Nao
-  if [ -d "$EXTERN_DIR/include/turbojpeg/" ]; then
+  if [ -d "$EXTERN_DIR/include/libjpeg-turbo/" ]; then
     echo "n"
     exit 0
   else
@@ -30,7 +30,7 @@ elif [ "$1" = "install" ]; then
   # check for yasm/nasm compiler
   WITH_SIMD="on" && (! type "nasm" &> /dev/null && ! type "yasm" &> /dev/null) && WITH_SIMD="off"
   # build ...
-  cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$EXTERN_DIR -DCMAKE_INSTALL_LIBDIR="$EXTERN_DIR/lib" -DCMAKE_INSTALL_INCLUDEDIR="$EXTERN_DIR/include/turbojpeg" -DWITH_SIMD=$WITH_SIMD .. && make && make install
+  cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$EXTERN_DIR -DCMAKE_INSTALL_LIBDIR="$EXTERN_DIR/lib" -DCMAKE_INSTALL_INCLUDEDIR="$EXTERN_DIR/include/libjpeg-turbo" -DWITH_SIMD=$WITH_SIMD .. && make && make install
   # finish
   cd ..  
 fi
