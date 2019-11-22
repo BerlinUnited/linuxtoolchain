f3c6226ac043e9d774ca460138fc3731951adbe4
Philipp Strobel <philippstrobel@posteo.de>
2019-11-22 10:05:57 +0100

    small cleanup

--- toolchain_native/extern/install_scripts/eigen.sh
+++ toolchain_native/extern/install_scripts/eigen.sh
@@ -18,7 +18,7 @@ if [ "$1" = "check" ]; then
     exit 1
   fi
 elif [ "$1" = "install" ]; then
-  rm -Rf glib-2.26.0
+  rm -Rf eigen-eigen-5a0156e40feb
   tar xvjf ../downloads/eigen-eigen-5a0156e40feb.tar.bz2
   cp -R eigen-eigen-5a0156e40feb/Eigen/ $EXTERN_DIR/include
 fi
